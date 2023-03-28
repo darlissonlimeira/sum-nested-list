cc55adde0a0ab29e23926a2f9f305fb758afbc94
Darlisson Limeira <49912598+drkcode@users.noreply.github.com>
2023-03-28 00:09:54 +0000

    refactor(sum_nested_number_list.py): create sum_number_list

--- sum_nested_number_lists.py
+++ sum_nested_number_lists.py
@@ -1,9 +1,19 @@
-from sum_number_list import sum_number_list
-
 def sum_nested_number_lists(list_of_lists_of_numbers):
     total = 0
 
     for list_of_numbers in list_of_lists_of_numbers:
         total += sum_number_list(list_of_numbers)
+
+    return total
+
+
+def sum_number_list(list_of_numbers):
+    total = 0
+
+    if len(list_of_numbers) < 1:
+        return total
+
+    for value in list_of_numbers:
+        total += value
 
     return total
